6e2a4ff35522fbbf8aff9553e7b0ecf0f0983472
Marie de Breuvand <marie.carette@gmail.com>
2023-12-19 00:37:49 +0100

    settingsscreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,28 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { IP_ADDRESS } from "../config.js";
 
-const IP_ADDRESS = '172.20.10.2'
 
 export default function FavoritesScreen({ navigation }) {
     const user = useSelector((state) => state.user.value);
 
     const [favoDrug, setFavoDrug] = useState([]);
 
-    fetch(`http://${IP_ADDRESS}:3000/favorites/addFavorites/${user.token}`)
+useEffect(() => {
+    fetch(`http://${IP_ADDRESS}:3000/favorites/loadFavorite/${user.token}`)
     .then((response) => response.json())
     .then((data) => {
-        setFavoDrug(data.favorites)
-    });
+        setFavoDrug(data.idAndName)
+    });},[]);
+
+    // A modifier
+    const handleDelete = (name) => {
+        fetch(`http://${IP_ADDRESS}:3000/favorites/deleteFavorite/${user.token}/${name}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setFavoDrug(data.idAndName)
+        });
+    }
 
     const favoDrugs = favoDrug.map((data, i) => {
         return (
           <View key={i} style={styles.card}>
-            <View>
+            <TouchableOpacity>
               <Text style={styles.name}>{data.name}</Text>
-            </View>
-            <FontAwesome name='trash-o' onPress={() => handleDelete(data.name)} size={25} color='#ec6e5b' />
+            </TouchableOpacity>
+            <FontAwesome style={styles.icon} name='trash-o' onPress={() => handleDelete(data.name)} size={25} color='#ec6e5b' />
           </View>
         );
       });
@@ -35,4 +45,32 @@ export default function FavoritesScreen({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({    
+  container: {
+  flex: 1,
+  padding: 20,
+  backgroundColor: '#fff',
+},
+title: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginBottom: 20,
+},
+card: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  backgroundColor: '#f0f0f0',
+  width: "80%",
+  padding: 10,
+  marginVertical: 10,
+  borderRadius: 5,
+  elevation: 3,
+},
+icon:{
+margin : 10,
+},
+name: {
+  fontSize: 18,
+  color: '#333',
+},});
