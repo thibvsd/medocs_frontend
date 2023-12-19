64b9eb281f5f348af165091541b9ffbc501c6d08
Thibaud Veyssade <tveyssade@gmail.com>
2023-12-19 17:24:11 +0100

    affichage des medicaments dans traitement

--- screens/TreatmentsScreen.js
+++ screens/TreatmentsScreen.js
@@ -1,64 +1,71 @@
 import React, { useState, useEffect } from "react";
-import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
 import Autocomplete from "react-native-autocomplete-input";
 import { IP_ADDRESS } from "../config.js";
 
 export default function TreatmentsScreen({ navigation }) {
-  const token = "kTe-BIKeY40kJaYz6JMm9sEcJFtpxVpD";
+  const token = "mYZ2UayAiPjfhaQRy0FXQH-1oktu1Xi9";
   const [suggestions, setSuggestions] = useState([]);
-  const [selectedDrugs, setSelectedDrugs] = useState([]);
   const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
   const [doseInput, setDoseInput] = useState("");
   const [med_reason, setMed_reason] = useState("");
+  const [drugAdd, setDrugAdd] = useState([]);
 
   useEffect(() => {
-    // Fetch les noms des médicaments (pour l'autocomplétion)
+    // AbortController pour arrêter la requête si query est modifié
+    const fetchDataController = new AbortController();
+    // Fonction pour fetch les noms des médicaments (pour l'autocomplétion)
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `http://${IP_ADDRESS}:3000/drugs/allNames`
-        );
-        const result = await response.json();
-        setData(result.namesAndId);
-        console.log(data);
+        if (query.length >= 3) {
+          const response = await fetch(
+            `http://${IP_ADDRESS}:3000/drugs/query3characters/${query}`,
+            { signal: fetchDataController.signal }
+          );
+          const result = await response.json();
+          setData(result.namesAndId);
+        }
       } catch (error) {
-        console.error(error);
+        if (error.name === "AbortError") {
+          console.log("La requête fetchData a été annulée.");
+        } else {
+          console.error(error);
+        }
       }
     };
-
     fetchData();
 
-  }, []);
+    return () => fetchDataController.abort();
+  }, [query]);
 
   // Sauvegarde les dosages et la raison médicale
   const onSave = () => {
-
     fetch(`http://${IP_ADDRESS}:3000/treatments/saveTreatment/${token}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         med_reason,
-        doseInput
+        doseInput,
       }),
-    })
+    });
   };
 
   const addDrugPress = async (drug) => {
-    const isSelected = data.find((selectedDrug) => selectedDrug === drug);
-    if (!isSelected) {
+    const isSelected = data.find((selectedDrug) => selectedDrug.name === drug);
+    if (isSelected._id) {
       try {
-        const response = await fetch(`http://${IP_ADDRESS}:3000/addDrugTreatment/${token}`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({_id: drug._id}),
-        });
-  
-        if (response.ok) {
-          const updatedDrug = await response.json();
-          setSelectedDrugs([...selectedDrugs, updatedDrug.drugTreatment]);
-        } else {
-          // Gérer les erreurs ou les cas non réussis
-          console.error("Erreur lors de l'ajout du médicament");
+        const response = await fetch(
+          `http://${IP_ADDRESS}:3000/treatments/addDrugTreatment/${token}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ _id: isSelected }),
+          }
+        );
+        const resJson = response.json();
+        if (resJson.result) {
+          loadDrugs(); // Recharge les médicaments
         }
       } catch (error) {
         // Gérer les erreurs réseau
@@ -66,6 +73,30 @@ export default function TreatmentsScreen({ navigation }) {
       }
     }
   };
+
+  useEffect(() => {
+    loadDrugs();
+  }, []);
+  
+    const loadDrugs = async () => {
+      try {
+        const response = await fetch(
+          `http://${IP_ADDRESS}:3000/treatments/${token}`
+        );
+        const data = await response.json();
+        // Récupère les noms des médicaments et les met à jour dans setDrugAdd
+        const drugNames = data.treatment.drugs.map((drug) => {
+          const match = drug.drug_id.name.match(/^([^,]+),/);
+          const cleanedName = match ? match[1] : drug.drug_id.name;
+          return cleanedName.trim();
+        });
+        console.log(drugNames);
+        setDrugAdd(drugNames);
+      } catch (error) {
+        // Gérer les erreurs réseau
+        console.error("Erreur réseau :", error);
+      }
+    };
 
   // Filtrer les suggestions en fonction de la valeur de l'input
   const filterData = (text) => {
@@ -79,6 +110,24 @@ export default function TreatmentsScreen({ navigation }) {
     }
   };
 
+  const drugTreatment = drugAdd.map((drug, index) => {
+    console.log("dans le jsx: " + drug);
+    return (
+      <View key={index} style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text>{drug}</Text>
+        <Text>Doses:</Text>
+        <TextInput
+          // value={drug.dose}
+          // onChangeText={(text) => handleDoseChange(text, drug.name)}
+          placeholder="0"
+        />
+        <TouchableOpacity onPress={() => removeDrug(drug.name)}>
+          {/* Icône de poubelle */}
+        </TouchableOpacity>
+      </View>
+    );
+  });
+
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.title}>Treatments in progress</Text>
@@ -89,6 +138,7 @@ export default function TreatmentsScreen({ navigation }) {
             data={suggestions}
             // value={query}
             onChangeText={(text) => {
+              setQuery(text);
               filterData(text);
             }}
             // Affiche les suggestions :
@@ -103,25 +153,7 @@ export default function TreatmentsScreen({ navigation }) {
             placeholder="Nom du médicament..."
             containerStyle={styles.autocompleteContainer}
           />
-          <View>
-            {selectedDrugs.map((drug, index) => (
-              <View
-                key={index}
-                style={{ flexDirection: "row", alignItems: "center" }}
-              >
-                <Text>{drug.name}</Text>
-                <Text>Doses:</Text>
-                <TextInput
-                  value={drug.dose}
-                  onChangeText={(text) => handleDoseChange(text, drug.name)}
-                  placeholder="Dose..."
-                />
-                <TouchableOpacity onPress={() => removeDrug(drug.name)}>
-                  {/* Icône de poubelle */}
-                </TouchableOpacity>
-              </View>
-            ))}
-          </View>
+          <View>{drugTreatment}</View>
         </View>
       </View>
       <View>
@@ -147,6 +179,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   drugsBox: {
-
-  }
+    height: '30%',
+    width: '90%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
 });
