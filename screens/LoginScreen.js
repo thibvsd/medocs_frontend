f3c2c8f8364b6d17a38a4e4958877565d612ccfa
Thibaud Veyssade <tveyssade@gmail.com>
2023-12-21 16:31:28 +0100

    mise a jour sign in popup alert si mdp ou email incorrect

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -118,6 +118,8 @@ export default function LoginScreen({ route, navigation }) {
           setSignInEmail("");
           setSignInPassword("");
           navigation.navigate("Profile");
+        } else {
+          Alert.alert(data.error);
         }
       });
   };
